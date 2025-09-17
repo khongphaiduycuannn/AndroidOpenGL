82a7ba621525ee41f35795188f35fb08e9430045
chuaterungxanh <quanndm@infinitytech.global>
2025-09-17 15:16:40 +0700

    feat[2d day night wallpaper]: add test resources, init gles

--- GLDayNightWallpaper/app/src/main/java/com/ndmquan/gl/daynight/wallpaper/MainActivity.kt
+++ GLDayNightWallpaper/app/src/main/java/com/ndmquan/gl/daynight/wallpaper/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.ndmquan.gl.daynight.wallpaper
 
+import android.opengl.GLSurfaceView
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ndmquan.gl.daynight.wallpaper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
